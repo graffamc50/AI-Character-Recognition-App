7d0b16e334fee8d310c321cc118a7217ad2a6a79
Colton Bradley Graffam <colton.b.graffam@gmail.com>
2023-06-20 20:36:01 -0700

    Dart Testing

---
+++ Dart Test.dart
@@ -0,0 +1,7 @@
+
+void main() {
+  print("Flutter is amazing");
+  print(
+      "Flutter Example apps repo is the collections of awesome apps built with flutter");
+import 'package:flutter/material.dart';
+void main() => runApp(MyApp());
