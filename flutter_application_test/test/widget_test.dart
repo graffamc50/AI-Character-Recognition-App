447470776602c1fc0b4dba429814b96dab17327f
Colton Graffam <135077476+graffamc50@users.noreply.github.com>
2023-06-28 18:00:59 -0700

    Update widget_test.dart

--- flutter_application_test/test/widget_test.dart
+++ flutter_application_test/test/widget_test.dart
@@ -1,5 +1,7 @@
 // This is a basic Flutter widget test.
 
+// im on vacation and need to keep profile active
+
 
 // To perform an interaction with a widget in your test, use the WidgetTester
 // utility in the flutter_test package. For example, you can send tap and scroll
